6448e4a27517141f106a42403962c8891ddb8ea5
Mirko Gueregat <mgueregath@gmail.com>
2020-11-17 23:53:44 -0300

    Jenkinsfile modified 16

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,12 +4,12 @@ echo 'Moving artifacts to /home/vps218/www'
 
 
 set -x
-cp build/* /home/vps218/www
+cp build/** /home/vps218/www/
 ls /home/vps218/www
 echo 'Kill container'
-sudo docker kill example-pipeline
+docker kill example-pipeline
 echo 'Serve build'
-sudo docker run -dit --name example-pipeline -p 8081:80 -v /home/vps218/www/:/usr/local/apache2/htdocs/ httpd:2.4
+docker run -dit --name example-pipeline -p 8081:80 -v /home/vps218/www/:/usr/local/apache2/htdocs/ httpd:2.4
 set +x
 
 echo 'Running container'
