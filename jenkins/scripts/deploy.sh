641a15ade851f45b0937d444062b926a8ac530a0
valrojo <alvaro.vavlevrde@alumnos.uach.cl>
2020-12-05 20:33:58 -0300

    Cambio de Ruta

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-echo 'Moving artifacts to /home/vps218/www'
+echo 'Moving artifacts to /home/vps218/www/merkit'
 
 
 set -x
-cp build/** /home/vps218/www/
-ls /home/vps218/www
+cp build/** /home/vps218/www/merkit/
+ls /home/vps218/www/merkit
 echo 'Kill container'
 docker kill example-pipeline
 echo 'Serve build'
-docker run -dit --name example-pipeline -p 8081:80 -v /home/vps218/www/:/usr/local/apache2/htdocs/ httpd:2.4
+docker run -dit --name example-pipeline -p 8081:80 -v /home/vps218/www/merkit/:/usr/local/apache2/htdocs/ httpd:2.4
 set +x
 
 echo 'Running container'
