34c65b6086c29f3ae5b709b69dec75dfb78566b8
Santasy <santasy98@gmail.com>
2021-01-15 09:21:01 -0300

    corrected jenkins script

--- jenkins/scripts/clean.sh
+++ jenkins/scripts/clean.sh
@@ -2,7 +2,7 @@ set +x
 echo 'Se limpian los contenedores Docker. En particular, se destruyen los utilizados en otras ramas.'
 set -x
 docker-compose down
+docker stop 8ece017e0be5 || true
+docker stop 2c5322c882c5 || true
+docker stop 8ad216b6f3d3 || true
 docker rm $(docker ps -aq -f "status=exited") || true
-docker rm 8ece017e0be5 || true
-docker rm 2c5322c882c5 || true
-docker rm 8ad216b6f3d3 || true
