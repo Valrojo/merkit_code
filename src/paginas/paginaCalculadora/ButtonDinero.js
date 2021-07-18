963c673c141c6ffa1d61943d9c0b3717cd53521b
dotpin <hackerter@gmail.com>
2021-07-18 00:13:18 -0400

    Contabiliza con redux

--- src/paginas/paginaCalculadora/ButtonDinero.js
+++ src/paginas/paginaCalculadora/ButtonDinero.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { sumaButton } from '../../actions/botones';
 
 
-export const ButtonDinero = ({price}) => {
+export const ButtonDinero = ({id,print,tipo,valor}) => {
 
-    const contador = 0;
+    //const contador = 0;    
+    const dispatch = useDispatch()
 
-    const [cnt, setCnt] = useState(contador)
-
-    const handle = () => {
-        if(price!="Icon Trash"){
+    
+    const [cnt, setCnt] = useState(1)
+    
+    const handleBtn = () => {
+        if(tipo!="Trash"){
             setCnt(cnt+1)
-            console.log(price+"  " +cnt);
-        }else{
-            setCnt(0)
+            console.log("numero: "+cnt);
+            dispatch(sumaButton({
+                id:id,
+                print:cnt,
+                tipo:tipo,
+                valor: valor
+            }
+
+            ))
         }
     }
 
@@ -22,23 +32,23 @@ export const ButtonDinero = ({price}) => {
         <div>
             <button type="button" 
                 className="btn btn-primary position-relative"
-                onClick={handle}
+                onClick={handleBtn}
             >
                 {
-                (price!="Icon Trash")
-                ?
+                    (tipo!="Trash")
+                    ?
+                        <>
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" 
+                                style={{color: 'white'}}
+                        >
+                            <a>{print}</a>
+                        </span>
+                            {tipo+" "+valor}
+                        </>
+                    :
                     <>
-                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" 
-                            style={{color: 'white'}}
-                    >
-                        <a>{cnt}</a>
-                    </span>
-                        {price}
+                        {tipo}
                     </>
-                :
-                <>
-                    {price}
-                </>
                 }
             </button>
             
